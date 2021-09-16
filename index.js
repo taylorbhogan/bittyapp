d06f08ed4a4c341e24e9e40387e322fa30fede1c
Taylor Hogan <taylorb.hogan@gmail.com>
2021-09-16 13:45:12 -0700

    Complete phase 1 minimal acceptable styling

--- index.js
+++ index.js
@@ -13,25 +13,31 @@ appName.innerHTML = apps[0]
 const toggleDark = () => {
   document.body.style.backgroundColor = '#000000'
   document.body.style.color = '#FFFFFF'
-  appName.style.color = '#F6F6F6'
+  appName.style.color = '#e013e7'
 }
 const toggleLight = () => {
   document.body.style.backgroundColor = '#FFFFFF'
-  document.body.style.color = '#FFFFFF'
-  appName.style.color = '#000000'
+  document.body.style.color = '#000000'
+  appName.style.color = '#13e7dd'
 }
 
 const rotateRight = () => {
+  if (apps[0] === 'grave') {
+    toggleLight()
+  }
   apps.push(apps.shift())
   appName.innerHTML = apps[0]
-  apps[0] === 'grave' ? toggleDark() : toggleLight();
+  apps[0] === 'grave' ? toggleDark() : null;
   return apps;
 }
 
 const rotateLeft = () => {
+  if (apps[0] === 'grave') {
+    toggleLight()
+  }
   apps.unshift(apps.pop())
   appName.innerHTML = apps[0]
-  apps[0] === 'grave' ? toggleDark() : toggleLight();
+  apps[0] === 'grave' ? toggleDark() : null;
   return apps;
 }
 
