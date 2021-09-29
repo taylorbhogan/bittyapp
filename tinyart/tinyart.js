044df653655b4a1978a8c152c3679dcc785c63aa
Taylor Hogan <taylorb.hogan@gmail.com>
2021-09-29 13:09:28 -0700

    File structure update

--- tinyart/tinyart.js
+++ tinyart/tinyart.js
@@ -5,9 +5,10 @@ let makeArt = () => {
   for (let x = 0 ; x < 255 ; x++){
     for (let y = 0 ; y < 255 ; y++){
       // if ((x ^ y) % 10) {
-      if ((x * y) % 5) {
+      if ((x ^ y) % 3) {
+      // if ((x * y) % 5) {
         context.fillRect(x*4, y*4, 4, 4)
-        context.fillStyle = `rgb(${x}, ${y}, 100)`
+        context.fillStyle = `rgb(255, ${x}, ${y})`
       }
     }
   }
