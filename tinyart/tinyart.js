4e594a734281207c7a71fb109172ba92c28c8e45
Taylor Hogan <taylorb.hogan@gmail.com>
2021-10-08 11:01:39 -0700

    Restructure file tree

--- tinyart/tinyart.js
+++ tinyart/tinyart.js
@@ -1,16 +1,77 @@
 let makeArt = () => {
   const tinyart = document.getElementById('tinyart')
-  const context = tinyart.getContext('2d')
+  const context = tinyart.getContext('2d')  // working in a 2d environment
 
-  for (let x = 0 ; x < 255 ; x++){
-    for (let y = 0 ; y < 255 ; y++){
-      // if ((x ^ y) % 10) {
+
+  // smooth gradiant
+  for (let x = -1; x < 255; x++) {
+    for (let y = -1; y < 255; y++) {
+      context.fillRect(x * 4, y * 4, 4, 4)
+      context.fillStyle = `rgb(255, ${x}, ${y})`
+    }
+  }
+  for (let x = -2; x < 500; x++) {
+    for (let y = -2; y < 500; y++) {
       if ((x ^ y) % 3) {
-      // if ((x * y) % 5) {
-        context.fillRect(x*4, y*4, 4, 4)
-        context.fillStyle = `rgb(255, ${x}, ${y})`
+
+        context.fillRect(x/((x ^ y) % 13), y / ((x ^ y) % 13), 4, 4)
+        context.fillStyle = `rgb(1, ${x}, ${y})`
       }
     }
   }
+
+
+  // neat squared overlay
+  // for (let x = -2; x < 255; x++) {
+  //   for (let y = -2; y < 255; y++) {
+  //     if ((x ^ y) % 3) {
+
+  //       context.fillRect(x*((x ^ y) % 13), y * ((x ^ y) % 13), 4, 4)
+  //       context.fillStyle = `rgb(1, ${x}, ${y})`
+  //     }
+  //   }
+  // }
+
+  // green drs office
+  // for (let x = -1; x < 255; x++) {
+  //   for (let y = -1; y < 255; y++) {
+  //     if ((x ^ y) % 10) {
+  //       context.fillRect((x ^ y) * 4, y * 4, 4, 4)
+  //       context.fillStyle = `rgb(${(x ^ y)}, ${x}, ${y})`
+  //     }
+  //   }
+  // }
+
+  // doctor's office
+  // for (let x = -1; x < 255; x++) {
+  //   for (let y = -1; y < 255; y++) {
+  //     if ((x ^ y) % 10) {
+  //       context.fillRect((x ^ y) * 4, y * 4, 4, 4)
+  //       context.fillStyle = `rgb(255, ${x}, ${y})`
+  //     }
+  //   }
+  // }
+
+  // my first
+  // for (let x = 0; x < 255; x++) {
+  //   for (let y = 0; y < 255; y++) {
+  //     if ((x ^ y) % 3) {
+  //       context.fillRect(x * 4, y * 4, 4, 4)
+  //       context.fillStyle = `rgb(255, ${x}, ${y})`
+  //     }
+  //   }
+  // }
+
+  // marks
+  // for (let x=0; x < 255; x++) {
+  // 	for (let y=0; y < 255; y++) {
+  // 		if ((x ^ y) % 10) {
+  // 			context.fillRect(x*((x ^ y) % 210), y*((x ^ y) % 210), x, y);
+  // 			context.fillStyle = `rgb(${(x ^ y) % 210}, ${y} ,${x} `;
+  // 		};
+  // 	};
+  // };
+  // }
+
 }
-makeArt();
+document.addEventListener('DOMContentLoaded', makeArt)
