8d83725af26b718e177014a6a06c91c5e0320fc2
Taylor Hogan <taylorb.hogan@gmail.com>
2021-10-14 18:12:39 -0700

    Implement edit note

--- bittynote/note.js
+++ bittynote/note.js
@@ -10,18 +10,65 @@ const generateNotes = () => {
   notebook.id = 'notebook'
   notebook.style.border = `4px solid ${apps[0].color}`
 
+  const editNote = (noteId, newNoteText) => {
+    deleteNote(noteId)
+    addNote(newNoteText)
+  }
+
+  const setEditForm = (noteId) => {
+
+    const formattedNotes = document.querySelectorAll('.note')
+    let divToReplace = null;
+    for (let i = 0; i < formattedNotes.length; i++) {
+      if (formattedNotes[i].id === noteId) {
+        divToReplace = formattedNotes[i]
+        break
+      }
+    }
+    if (divToReplace) {
+      const oldNote = divToReplace.firstChild.textContent;
+
+      while (divToReplace.firstChild) {
+        divToReplace.removeChild(divToReplace.lastChild)
+      }
+
+      const form = document.createElement('form')
+      form.id = 'editForm'
+      const input = document.createElement('input')
+      input.type = 'text'
+      input.value = oldNote;
+      const submit = document.createElement('input')
+      submit.type = 'submit'
+      submit.innerText = 'save'
+
+      form.append(input)
+      form.append(submit)
+
+      form.addEventListener('submit', e => {
+        e.preventDefault();
+        const text = input.value.trim()
+
+        editNote(noteId, text)
+      })
+
+      divToReplace.append(form)
+
+    } else {
+      errorDisplay.textContent = 'An error occurred while editing your note.'
+    }
+  }
 
   const deleteNote = (noteId) => {
     const savedNotes = JSON.parse(localStorage.getItem('notes'))
 
     let deletedNote = null;
-    for (let i = 0; i < savedNotes.length; i++){
-      if (savedNotes[i].id === +noteId){
+    for (let i = 0; i < savedNotes.length; i++) {
+      if (savedNotes[i].id === +noteId) {
         deletedNote = savedNotes.splice(i, 1)
         break
       }
     }
-    if (deletedNote){
+    if (deletedNote) {
       localStorage.setItem('notes', JSON.stringify(savedNotes))
       renderNotes()
     } else {
@@ -64,8 +111,13 @@ const generateNotes = () => {
     })
 
     const editButton = document.createElement('button')
+    editButton.id = note.id
     editButton.classList.add('noteButton')
+    editButton.classList.add('editButton')
     editButton.textContent = 'edit'
+    editButton.addEventListener('click', e => {
+      setEditForm(e.target.id)
+    })
 
     buttonDiv.appendChild(editButton)
     buttonDiv.appendChild(deleteButton)
@@ -83,7 +135,7 @@ const generateNotes = () => {
     if (savedInStorage) {
       const savedNotes = JSON.parse(savedInStorage)
 
-      while (notebook.firstChild){
+      while (notebook.firstChild) {
         notebook.removeChild(notebook.lastChild)
       }
       savedNotes.reverse().forEach(note => {
