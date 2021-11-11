e034a8f4ae0e3b223a5b0f1469eeafab3832d95a
Taylor Hogan <taylorb.hogan@gmail.com>
2021-11-11 12:38:16 -0800

    Improve styling

---
+++ app.js
@@ -0,0 +1,7 @@
+const generateApp = () => {
+  const display = document.getElementById('display')
+  const p = document.createElement('p')
+  p.textContent = 'welcome'
+  p.classList.add('coming-soon')
+  display.append(p)
+}
