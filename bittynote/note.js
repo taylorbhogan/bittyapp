81f0e838359ab0f3d4c211711ffa491a02d18d05
Taylor Hogan <taylorb.hogan@gmail.com>
2021-11-29 14:54:10 -0800

    Improve readability

--- bittynote/note.js
+++ bittynote/note.js
@@ -2,8 +2,127 @@ const generateNotes = () => {
   const notebook = document.createElement('div')
   notebook.id = 'notebook'
   notebook.style.border = `4px solid ${apps[0].color}`
+  display.append(notebook)
+
+
+  //////  Note Formatter for use in renderNotes()  //////
+  const formatNote = note => {
+    ////  Create Note Div
+    const formattedNote = document.createElement('div')
+    formattedNote.id = note.id
+    formattedNote.classList.add('note')
+
+    ////  Create Note Text
+    const noteText = document.createElement('p')
+    noteText.classList.add('noteText')
+    noteText.textContent = note.text
+
+    ////  Create Bottom Row
+    const adminContainer = document.createElement('div')
+    adminContainer.classList.add('noteAdmin')
+
+    const timestamp = document.createElement('p')
+    const theDate = new Date(note.id).toLocaleDateString()
+    const theTime = new Date(note.id).toLocaleTimeString()
+    timestamp.textContent = `${theTime} on ${theDate}`
+    adminContainer.appendChild(timestamp)
+
+    ////  Create Buttons
+    const buttonDiv = document.createElement('div')
+    const deleteButton = document.createElement('button')
+    deleteButton.id = note.id
+    deleteButton.textContent = 'erase'
+    deleteButton.addEventListener('click', e => {
+      deleteNote(e.target.id);
+    })
+
+    const editButton = document.createElement('button')
+    editButton.id = note.id
+    editButton.textContent = 'edit'
+    editButton.addEventListener('click', e => {
+      setEditForm(e.target.id)
+    })
+
+    buttonDiv.append(editButton, deleteButton)
+    adminContainer.append(buttonDiv)
+
+    formattedNote.append(noteText, adminContainer)
+    return formattedNote;
+  }
+
+  const renderNotes = () => {
+    const savedInStorage = localStorage.getItem('notes')
+
+    if (savedInStorage) {
+      const savedNotesObj = JSON.parse(savedInStorage)
+
+      notebook.innerHTML = ''
+      const savedNotes = Object.values(savedNotesObj)
+      savedNotes.reverse().forEach(note => {
+        const formattedNote = formatNote(note)
+        notebook.append(formattedNote)
+      })
+    }
+  }
+  renderNotes()
+
 
   const errorDisplay = document.getElementById('errors')
+
+  //////  Create Form  //////
+  const newNoteForm = document.createElement('form')
+  newNoteForm.id = 'noteForm';
+
+  const input = document.createElement('input')
+  input.type = 'text'
+  input.classList.add('noteInput')
+  input.placeholder = 'Enter your note...'
+
+  const button = document.createElement('button')
+  button.classList.add('submitButton')
+  button.type = 'submit'
+  button.textContent = 'Jot'
+  button.style.fontFamily = apps[0].font
+  button.style.color = apps[0].color
+
+  newNoteForm.addEventListener('submit', e => {
+    e.preventDefault()
+
+    const text = input.value.trim();      // always a good idea to trim off any whitespace
+    addNote(text)
+
+    input.value = ''
+  })
+
+  newNoteForm.append(input, button)
+  options.append(newNoteForm)
+
+
+  //////  Note Interaction Functions  //////
+  const addNote = text => {
+    const note = {
+      id: Date.now(),
+      text,
+    }
+
+    let savedNotes = JSON.parse(localStorage.getItem('notes'))
+    if (savedNotes) {
+      savedNotes[note.id] = note
+    } else {
+      savedNotes = {};
+      savedNotes[note.id] = note;
+    }
+    localStorage.setItem('notes', JSON.stringify(savedNotes))
+
+    renderNotes()
+  }
+
+  const deleteNote = (noteId) => {
+    const savedNotes = JSON.parse(localStorage.getItem('notes'))
+    delete savedNotes[noteId]
+    localStorage.setItem('notes', JSON.stringify(savedNotes))
+    renderNotes()
+  }
 
   const editNote = (noteId, newNoteText) => {
     deleteNote(noteId)
@@ -56,124 +175,4 @@ const generateNotes = () => {
       errorDisplay.textContent = 'An error occurred while editing your note.'
     }
   }
-
-  const deleteNote = (noteId) => {
-    const savedNotes = JSON.parse(localStorage.getItem('notes'))
-    delete savedNotes[noteId]
-    localStorage.setItem('notes', JSON.stringify(savedNotes))
-    renderNotes()
-  }
-
-  const formatNote = note => {
-    //////  Create Note Div  //////
-    const formattedNote = document.createElement('div')
-    formattedNote.id = note.id
-    formattedNote.classList.add('note')
-
-
-    //////  Create Note Text  //////
-    const noteText = document.createElement('p')
-    noteText.classList.add('noteText')
-    noteText.textContent = note.text
-
-
-    //////  Create Bottom Row  //////
-    const adminContainer = document.createElement('div')
-    adminContainer.classList.add('noteAdmin')
-
-    const timestamp = document.createElement('p')
-    const theDate = new Date(note.id).toLocaleDateString()
-    const theTime = new Date(note.id).toLocaleTimeString()
-    timestamp.textContent = `${theTime} on ${theDate}`
-    adminContainer.appendChild(timestamp)
-
-    //////  Create Buttons  //////
-    const buttonDiv = document.createElement('div')
-    const deleteButton = document.createElement('button')
-    deleteButton.id = note.id
-    deleteButton.textContent = 'erase'
-    deleteButton.addEventListener('click', e => {
-      deleteNote(e.target.id);
-    })
-
-    const editButton = document.createElement('button')
-    editButton.id = note.id
-    editButton.textContent = 'edit'
-    editButton.addEventListener('click', e => {
-      setEditForm(e.target.id)
-    })
-
-    buttonDiv.append(editButton, deleteButton)
-    adminContainer.append(buttonDiv)
-
-
-    formattedNote.append(noteText, adminContainer)
-    return formattedNote;
-  }
-
-  const addNote = text => {
-    //////  add note to storage, then call renderNotes  //////
-    const note = {
-      id: Date.now(),
-      text,
-    }
-
-    let savedNotes = JSON.parse(localStorage.getItem('notes'))
-    if (savedNotes) {
-      savedNotes[note.id] = note
-    } else {
-      savedNotes = {};
-      savedNotes[note.id] = note;
-    }
-    localStorage.setItem('notes', JSON.stringify(savedNotes))
-
-    renderNotes()
-  }
-
-  //////  Create Form  //////
-  const form = document.createElement('form')
-  form.id = 'noteForm';
-
-  const input = document.createElement('input')
-  input.type = 'text'
-  input.classList.add('noteInput')
-  input.placeholder = 'Enter your note...'
-
-  const button = document.createElement('button')
-  button.classList.add('submitButton')
-  button.type = 'submit'
-  button.textContent = 'Jot'
-  button.style.fontFamily = apps[0].font
-  button.style.color = apps[0].color
-
-  form.addEventListener('submit', e => {
-    e.preventDefault()
-
-    const text = input.value.trim();      // always a good idea to trim off any whitespace
-    addNote(text)
-
-    input.value = ''
-  })
-
-  form.append(input, button)
-  options.append(form)
-
-  display.append(notebook)
-
-
-  const renderNotes = () => {
-    const savedInStorage = localStorage.getItem('notes')
-
-    if (savedInStorage) {
-      const savedNotesObj = JSON.parse(savedInStorage)
-
-      notebook.innerHTML = ''
-      const savedNotes = Object.values(savedNotesObj)
-      savedNotes.reverse().forEach(note => {
-        const formattedNote = formatNote(note)
-        notebook.append(formattedNote)
-      })
-    }
-  }
-  renderNotes()
 }
